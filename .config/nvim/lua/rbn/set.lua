99dd4b3f565c98443c4832e616251ea1b4ff1027
rbn <richnyman@gmail.com>
2023-08-30 16:07:30 -0400

    nvim

---
+++ .config/nvim/lua/rbn/set.lua
@@ -0,0 +1,32 @@
+vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.guicursor = "a:blinkon100"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 3
+vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
